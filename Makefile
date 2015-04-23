e9fdc24af5cd252304baf3ac40f7d24e38bce978
Manfred Touron <m@42.am>
2015-04-23 13:15:04 +0200

    Improved build system for OS X

---
+++ Makefile
@@ -0,0 +1,18 @@
+NAME = "IRC Cloud"
+
+.PHONY: build open
+
+build:
+	node build.js
+	# cp assets/mac-ffmpegsumo.so build/$(NAME)/osx64/$(NAME).app/Contents/Frameworks/nwjs\ Framework.framework/Libraries/ffmpegsumo.so
+
+release:
+	# OSX
+	#-rm -f build/$(NAME).dmg
+	# appdmg assets/dmg.json build/$(NAME).dmg
+
+	# WINDOWS
+	# makensis ./assets/installer.nsi
+
+open:
+	open ./build/$(NAME)/osx64/$(NAME).app
--- build.js
+++ build.js
@@ -2,9 +2,13 @@ var NwBuilder = require('node-webkit-builder');
 
 var nw = new NwBuilder({
   files: './src/**',
-  platforms: ['osx'],
-  macIcns: './src/icon.icns',
-  macZip: true
+  platforms: ['osx64'],
+  macIcns: './assets/IRCCloud.icns',
+  macZip: true,
+  macPlist: {
+    NSHumanReadableCopyright: 'Copyright © 2015 github.com/moul',
+    CFBundleIdendifier: 'com.github.moul.irccloud-desktop-app'
+  }
 });
 
 nw.on('log', console.log);
